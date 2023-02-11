eaa7cda3aad7acf2dd7cfaff6ac01a8d14b7a99c
Yukta269 <96491851+Yukta269@users.noreply.github.com>
2023-02-11 13:54:08 +0000

    Update roll-a-dice.py

--- roll-a-dice.py
+++ roll-a-dice.py
@@ -1,52 +1,49 @@
 import random
-print("Roll a dice")
-dice = input("Do you want to roll dice or not?")
+
 response = "y"
 while response == ("y"):
-    print("Enter y to continue")
+    no = random.randint(1,6)
 
+    if no == 1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
 
-no = random.randint(1,6)
-if no == 1:
-    print("[-----]")
-    print("[     ]")
-    print("[  0  ]")
-    print("[     ]")
-    print("[-----]")
+    if no == 2: 
+        print("[-----]") 
+        print("[ 0   ]") 
+        print("[     ]") 
+        print("[   0 ]") 
+        print("[-----]") 
+    if no == 3: 
+        print("[-----]") 
+        print("[     ]") 
+        print("[0 0 0]") 
+        print("[     ]") 
+        print("[-----]") 
+    if no == 4: 
+        print("[-----]") 
+        print("[0   0]") 
+        print("[     ]") 
+        print("[0   0]") 
+        print("[-----]") 
+    if no == 5: 
+        print("[-----]") 
+        print("[0   0]") 
+        print("[  0  ]") 
+        print("[0   0]") 
+        print("[-----]") 
+    if no == 6: 
+        print("[-----]") 
+        print("[0 0 0]") 
+        print("[     ]") 
+        print("[0 0 0]") 
+        print("[-----]") 
+          
+          
+    response=input("press y to roll again and n to exit:") 
+    print("\n") 
 
-if no == 2:
-    print("[-----]")
-    print("[     ]")
-    print("[  0  ]")
-    print("[     ]")
-    print("[-----]")
-
-if no == 3:
-    print("[-----]")
-    print("[     ]")
-    print("[  0  ]")
-    print("[     ]")
-    print("[-----]")
-
-if no == 4:
-    print("[-----]")
-    print("[     ]")
-    print("[  0  ]")
-    print("[     ]")
-    print("[-----]")
-
-if no == 5:
-    print("[-----]")
-    print("[     ]")
-    print("[  0  ]")
-    print("[     ]")
-    print("[-----]")
-
-if no == 6:
-    print("[-----]")
-    print("[     ]")
-    print("[  0  ]")
-    print("[     ]")
-    print("[-----]")
-
-response = input("press y to continue and n to exit")
+    
